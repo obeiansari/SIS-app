a8a9489412e03fd05beb54c1bf930ff2af138d18
obeiansari <mohdobeiansari@gmail.com>
2021-01-22 01:43:24 +0530

    Flikering is solver using headers

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const CONSTANTS = {
 }
 
 function isAuthorized() {
-    const user = localStorage.getItem(CONSTANTS.LOCAL_STORAGE_KEY);
+    const user = JSON.parse(localStorage.getItem(CONSTANTS.LOCAL_STORAGE_KEY));
     if (!user) {
         location.href = `${CONSTANTS.APP_URL}login.html`;
     } else {
@@ -18,7 +18,6 @@ function isAuthorized() {
                 "Authorization": `Bearer ${user.token}`
             },
             success: () => {
-                // location.href = `${CONSTANTS.APP_URL}dashboard.html`;
                 $('body').removeClass('d-none');
             },
             error: (error) => {
